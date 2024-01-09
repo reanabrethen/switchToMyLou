f23ce0ddcfefba88bcfc468406529d88c0f48b83
Admin <reanajb@gmail.com>
2024-01-09 10:48:07 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+/******************************/
+//Your Code Here 
+
+
+
+/****************************/
+// Our code here. Don't touch!
+if (typeof dayOfWeek === 'undefined') {
+    dayOfWeek = undefined
+  }
+  
+  if (typeof identifySeason === 'undefined') {
+    identifySeason = undefined
+  }
+  
+  if (typeof menuSelection === 'undefined') {
+    menuSelection = undefined
+  }
+
+  
+  
+  module.exports = {
+    dayOfWeek, 
+    identifySeason, 
+    menuSelection
+  }
+  
